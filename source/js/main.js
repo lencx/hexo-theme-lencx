6c770cceda71b845cf377aca27947acefb3eba5a
Len <cxin1314@gmail.com>
2017-03-16 18:54:40 +0800

    init# tags theme

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,30 @@
 // import '../css/main.scss'
 'use strict'
+/**
+ * Random Color
+ * @author lencx
+ * @param {Number} saturation => 0 - 100
+ * @param {Number} lightness => 0 - 100
+ * @return {String} hsla()
+ */
+function randomColor (s, l) {
+    let deg, random, alpha
+    deg = Math.ceil(Math.random()*360)
+    random = Math.random()
+    alpha = random < .4 ? .7 : random
+    return 'hsla('+deg+', '+s+'%, '+l+'%,'+alpha+')'
+}
+/**
+ * Unique values in an array
+ * @author lencx
+ * @param {Array} arr
+ */
+function unique(arr) {
+    return arr.filter((item, index, arr) => {
+        return arr.indexOf(item) === index
+    })
+}
+
 // header 
 !function() {
     // menu
@@ -167,11 +192,85 @@ $(function(){
 $(function(){
     let $tag = $('.tagCloud').find('a')
     $tag.each((i) => {
-        let random, deg, alpha, color
-        random = Math.random().toFixed(2)
-        deg = Math.ceil(Math.random()*360)
-        alpha = random < .4 ? .8 : random
-        color = 'hsla('+deg+', 70%, 60%,'+alpha+')'
-        $($tag[i]).css('color', color)
+        let fontSize, vertical, rx, ry, x, y, style
+        console.log(randomColor(65, 72))
+        // console.log($($tag[i]).css('font-size').split('px')[0])
+        fontSize = $($tag[i]).css('font-size').split('px')[0]
+        rx = Math.ceil(Math.random()*10)
+        ry = Math.ceil(Math.random()*10)
+
+        x = rx<4?rx+'px':-2+'px '
+        y = ry<4?ry+'px':-1+'px '
+        vertical = fontSize < 28
+            ? Math.random()>.5?Math.ceil(Math.random()*16):-3
+            : ''
+        style = {
+            'box-shadow': 'inset '+x+y+'8px '+randomColor(70, 60),
+            'border-radius': fontSize < 24 ? Math.ceil(Math.random()*8) : Math.ceil(Math.random()*12),
+            'color': randomColor(70, 60),
+            'vertical-align': vertical
+        }
+        $($tag[i]).css(style)
     })
 })
+
+// tags icon
+$(function() {
+    let $tags, tagList, tagArr
+    $tags = $('.post-meta .tag')
+    tagList = []
+    $tags.each(i => {
+        let _this = $($tags[i])
+        tagList.push(_this.html())
+    })
+    tagArr = unique(tagList)
+    let tags = document.querySelectorAll('.tag')
+    for(let i=0, len=tagArr.length; i<len; i++) {
+        let _this = tagArr[i]
+        let c = randomColor(80, 60)
+        $tags.each(j => {
+            let __this = $($tags[j])
+            if(__this.html() === _this) {
+                let deg = Math.random() > .5
+                    ? -Math.ceil(Math.random()*4)
+                    : Math.ceil(Math.random()*12)
+                __this.css({
+                    'background': c,
+                    'transform': 'rotate('+deg+'deg)'
+                })
+                __this.find('i').css('background', c)
+                
+            }
+        })
+    }
+})
+
+// sidebar scroll
+// $(function() {
+//     let $sidebar = $('aside.sidebar')
+//     let sidebarTop = $sidebar.offset().top
+//     let state = {
+//         start: {
+//             'position': 'fixed',
+//             'top': '60px',
+//             'right': '50px',
+//             'overflower': 'auto',
+//             'height': '80vh',
+//             'overflow-y': 'scroll',
+//             'bottom': '50px'
+//         },
+//         end: {
+//             'position': 'absolute',
+//             'top': '30px',
+//             'right': 0,
+//             'height': '100vh',
+//             'overflow': 'hidden'
+//         }
+//     }
+//     $(window).on('scroll', () => {
+//         let scrollTop = $(window).scrollTop()
+//         scrollTop > sidebarTop
+//             ? $sidebar.css(state.start)
+//             : $sidebar.css(state.end)
+//     })
+// })
